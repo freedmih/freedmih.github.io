6d92c8756c34bccc32bbe1e8138ff0e40af932e8
freedmih <kozlenkomichael@gmail.com>
2022-03-01 18:05:30 +0300

    Tasks sort

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,7 @@ import downArrow from './downArrow.svg';
 
 import Task from "./components/Task";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
@@ -13,25 +13,44 @@ function App() {
   const [titleTask, setTitleTask] = useState('');
 
   const removeTask = taskId => {
-
-    console.log(tasks, taskId);
     setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
   }
+
+  useEffect(() => {
+    setTasks(Array.from(Array(10).keys()).map(x => ({
+      id: x,
+      title: `RANDOM${x}`,
+      date: `${new Date().getDay()+x}/${new Date().getMonth()}/${new Date().getFullYear()}`,
+      deleteCallback: removeTask
+    })));
+  }, []);
 
   const addTask = event => {
 
     if(titleTask.trim() === "") return;
 
     if(event.key === "Enter") {
+
+      let date = new Date();
+      let dateStr = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+
       setTasks(
         [
           ...tasks,
-          { id: tasks.length, title: titleTask, deleteCallback: removeTask }
+          { id: tasks.length, title: titleTask, deleteCallback: removeTask, date: dateStr }
         ]
       );
 
       setTitleTask('');
     }
+  }
+
+  const sortTasksUp = () => {
+    setTasks(prevTasks => prevTasks.sort((a, b) => b.id - a.id))
+  }
+
+  const sortTasksDown = () => {
+    setTasks(prevTasks => prevTasks.sort((a, b) => a.id - b.id))
   }
 
   return (
@@ -48,13 +67,13 @@ function App() {
         </div>
         <div className="sort-buttons">
           <p>Sort by Date</p>
-          <img src={upArrow} className="icon" alt="Sort by up date"/>
-          <img src={downArrow} className="icon icon-up" alt="Sort by down date"/>
+          <button onClick={() => sortTasksUp()}><img src={upArrow} className="icon" alt="Sort by down date"/></button>
+          <button onClick={() => sortTasksDown()}><img src={downArrow} className="icon icon-up" alt="Sort by up date"/></button>
         </div>
       </div>
       <div className="Tasks">
         {
-          tasks.map(task => <Task key={task.id} id={task.id} title={task.title} date={task.date} deleteCallback={removeTask} />)
+          tasks.slice(0, 10).map(task => <Task key={task.id} id={task.id} title={task.title} date={task.date} deleteCallback={removeTask} />)
         }
       </div>
     </div>

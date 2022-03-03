b1cd7404a88558ff7c7912e66875a25abade8beb
freedmih <kozlenkomichael@gmail.com>
2022-03-03 12:45:05 +0300

    Fixed className and added key to render component

--- front-end/src/components/Pagination.jsx
+++ front-end/src/components/Pagination.jsx
@@ -1,17 +1,17 @@
 export default function Pagination({ count, activePage, setActivePage }) {
     let pageButtons = [];
-
+    
     let start = activePage > 1 ? activePage - 2 : 0
     let lastPage = Math.ceil(count / 5);
 
-    let end = start + 5 > lastPage ? lastPage : start + 5
+    let end = start + 5 > lastPage ? lastPage : start + 5;
 
     for (let i = start; i < end; i++) {
-        pageButtons.push(<button className={i === activePage ? "button-pag-selected" : ""} onClick={() => setActivePage(i)}>{i + 1}</button>)
+        pageButtons.push(<button key={i} className={i === activePage ? "button-pag-selected" : ""} onClick={() => setActivePage(i)}>{i + 1}</button>)
     }
 
     return (
-        <div class="pagination">
+        <div className="pagination">
             <button onClick={() => setActivePage(0)}>{'<<'}</button>
             <button disabled={activePage <= 0} onClick={() => setActivePage(activePage - 1)}>{'<'}</button>
             {pageButtons}

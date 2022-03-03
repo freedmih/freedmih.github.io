cb859bd1f217533c92c0c5c0edf885692ab5322a
freedmih <kozlenkomichael@gmail.com>
2022-03-03 12:57:01 +0300

    React.memo for pagination buttons

--- front-end/src/components/Pagination.jsx
+++ front-end/src/components/Pagination.jsx
@@ -1,26 +1,36 @@
+import { useMemo } from "react";
+
 export default function Pagination({ count, activePage, setActivePage }) {
-    let pageButtons = [];
+
+    const buttonsInfo = useMemo(() => {
+        const pageButtons = [];
+
+        let start = activePage > 1 ? activePage - 2 : 0;
+        let lastPage = Math.ceil(count / 5);
     
-    let start = activePage > 1 ? activePage - 2 : 0
-    let lastPage = Math.ceil(count / 5);
+        let end = start + 5 > lastPage ? lastPage : start + 5;
+    
+        if(lastPage > 4 && end - start < 5) {
+            start = end - 5;
+        }
 
-    let end = start + 5 > lastPage ? lastPage : start + 5;
+        for (let i = start; i < end; i++) {
+            pageButtons.push(<button key={i} className={i === activePage ? "button-pag-selected" : ""} onClick={() => setActivePage(i)}>{i + 1}</button>)
+        } 
 
-    if(lastPage > 4 && end - start < 5) {
-        start = end - 5;
-    }
-
-    for (let i = start; i < end; i++) {
-        pageButtons.push(<button key={i} className={i === activePage ? "button-pag-selected" : ""} onClick={() => setActivePage(i)}>{i + 1}</button>)
-    }
+        return {
+            buttons: pageButtons,
+            lastPage
+        }
+    }, [activePage, count]);
 
     return (
         <div className="pagination">
             <button onClick={() => setActivePage(0)}>{'<<'}</button>
             <button disabled={activePage <= 0} onClick={() => setActivePage(activePage - 1)}>{'<'}</button>
-            {pageButtons}
-            <button disabled={activePage >= lastPage - 1} onClick={() => setActivePage(activePage + 1)}>{'>'}</button>
-            <button onClick={() => setActivePage(lastPage - 1)}>{'>>'}</button>
+            {buttonsInfo.buttons}
+            <button disabled={activePage >= buttonsInfo.lastPage - 1} onClick={() => setActivePage(activePage + 1)}>{'>'}</button>
+            <button onClick={() => setActivePage(buttonsInfo.lastPage - 1)}>{'>>'}</button>
         </div>
     )
 }

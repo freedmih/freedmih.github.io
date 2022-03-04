d8098ee78e89eb9af049ab28ed10111f733caeb9
freedmih <kozlenkomichael@gmail.com>
2022-03-04 14:11:50 +0300

    Added useEffect instead of forward call

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,7 +14,7 @@ import FilterButtons from './components/FilterButtons';
 import FormInput from './components/FormInput';
 import { GetIntDateNow } from './utils/date';
 
-import { message, Button, Space } from 'antd';
+import { message } from 'antd';
 
 const error = text => {
   message.error(text);
@@ -25,8 +25,9 @@ function App() {
   const [index, setIndex] = useState(0);
   const [filter, setFilter] = useState(Constants.FILTER_ALL);
   const [page, setPage] = useState(Constants.FIRST_PAGE_INDEX);
+  const [sortType, setSortType] = useState(Constants.DATE_FILTER_DIRECTION_DOWN);
 
-  const [ sortType, setSortType ] = useState(Constants.DATE_FILTER_DIRECTION_DOWN);
+  const [limitedTasks, setLimitedTasks] = useState([]);
 
   const {
     todos,
@@ -39,8 +40,49 @@ function App() {
     isValidTitle
   } = useTaskState([]);
 
+  const filteredTasks = () => {
+    switch (filter) {
+      case Constants.FILTER_ALL:
+        return todos;
+      case Constants.FILTER_DONE:
+        return getOnlyDoneTasks();
+      case Constants.FILTER_UNDONE:
+        return getOnlyUnDoneTasks();
+      default:
+        return todos;
+    }
+  }
+
+  const getTasksByPage = (page) => {
+    return limitedTasks.slice(page * Constants.MAX_TASKS_PER_PAGE, page * Constants.MAX_TASKS_PER_PAGE + Constants.MAX_TASKS_PER_PAGE);
+  }
+
   useEffect(() => sortByDate(sortType), [sortType]);
 
+  useEffect(() => {
+    setPage(Constants.FIRST_PAGE_INDEX);
+    setLimitedTasks(filteredTasks());
+  }, [filter]);
+
+  useEffect(() => {
+    setLimitedTasks(filteredTasks());
+  }, [todos]);
+
+  const isEmptyPage = () => {
+    const countOfTasks = limitedTasks.slice(page * Constants.MAX_TASKS_PER_PAGE, page * Constants.MAX_TASKS_PER_PAGE + Constants.MAX_TASKS_PER_PAGE).length;
+
+    if (countOfTasks === 0 && page > 0) {
+      return true;
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    if (isEmptyPage()) {
+      setPage(page => page - 1);
+    }
+  }, [limitedTasks]);
 
   const addTask = titleTask => {
 
@@ -64,50 +106,14 @@ function App() {
     return true;
   }
 
-  const changeFilter = current => {
-    setPage(Constants.FIRST_PAGE_INDEX);
-    setFilter(current);
-  }
-
-  const filteredTasks = () => {
-    switch (filter) {
-      case Constants.FILTER_ALL:
-        return todos;
-      case Constants.FILTER_DONE:
-        return getOnlyDoneTasks();
-      case Constants.FILTER_UNDONE:
-        return getOnlyUnDoneTasks();
-      default:
-        return todos;
-    }
-  }
-
-  const getTasksByPage = page => {
-    return filteredTasks().slice(page * Constants.MAX_TASKS_PER_PAGE, page * Constants.MAX_TASKS_PER_PAGE + Constants.MAX_TASKS_PER_PAGE);
-  }
-
-  const footer = filteredTasks().length > Constants.MAX_TASKS_PER_PAGE ? <Pagination count={filteredTasks().length} activePage={page} setActivePage={setPage} /> : <></>
-
-  const isEmptyPage = () => {
-    const countOfTasks = filteredTasks().slice(page * Constants.MAX_TASKS_PER_PAGE, page * Constants.MAX_TASKS_PER_PAGE + Constants.MAX_TASKS_PER_PAGE).length;
-
-    if (countOfTasks === 0 && page > 0) {
-      return true;
-    }
-
-    return false;
-  }
-
-  if (isEmptyPage()) {
-    setPage(page => page - 1);
-  }
+  const footer = limitedTasks.length > Constants.MAX_TASKS_PER_PAGE ? <Pagination count={limitedTasks.length} activePage={page} setActivePage={setPage} /> : <></>
 
   return (
     <div className="App">
       <h1>Todo</h1>
       <FormInput addTask={addTask} />
       <div className="control-container">
-        <FilterButtons filter={filter} changeFilter={changeFilter} />
+        <FilterButtons filter={filter} setFilter={setFilter} />
         <SortButtons setSortType={setSortType} />
       </div>
       <TaskList isValidTitle={isValidTitle} deleteTask={deleteTodo} updateTodo={updateTodo} tasks={getTasksByPage(page)} />
--- front-end/src/components/FilterButtons.jsx
+++ front-end/src/components/FilterButtons.jsx
@@ -2,13 +2,13 @@ import { Constants } from "../constants";
 
 const GetFilterStyle = selected => selected ? "control-button control-button-selected" : "control-button";
 
-export default function FilterButtons({ filter, changeFilter }) {
+export default function FilterButtons({ filter, setFilter }) {
 
     return (
         <div className="control-buttons">
-            <button className={GetFilterStyle(filter === Constants.FILTER_ALL)} onClick={() => changeFilter(Constants.FILTER_ALL)}>All</button>
-            <button className={GetFilterStyle(filter === Constants.FILTER_DONE)} onClick={() => changeFilter(Constants.FILTER_DONE)}>Done</button>
-            <button className={GetFilterStyle(filter === Constants.FILTER_UNDONE)} onClick={() => changeFilter(Constants.FILTER_UNDONE)}>Undone</button>
+            <button className={GetFilterStyle(filter === Constants.FILTER_ALL)} onClick={() => setFilter(Constants.FILTER_ALL)}>All</button>
+            <button className={GetFilterStyle(filter === Constants.FILTER_DONE)} onClick={() => setFilter(Constants.FILTER_DONE)}>Done</button>
+            <button className={GetFilterStyle(filter === Constants.FILTER_UNDONE)} onClick={() => setFilter(Constants.FILTER_UNDONE)}>Undone</button>
         </div>
     )
 }

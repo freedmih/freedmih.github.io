0d13a54b68f3a023e39e4b44eecd56469df96769
freedmih <kozlenkomichael@gmail.com>
2022-03-05 12:15:01 +0300

    Sort Buttons

--- front-end/src/components/SortButtons.jsx
+++ front-end/src/components/SortButtons.jsx
@@ -3,12 +3,19 @@ import downArrow from './../svg/downArrow.svg';
 
 import { Constants } from '../constants';
 
+import { Typography, Space } from 'antd';
+
+import { ArrowUpOutlined } from '@ant-design/icons'
+
+const { Text, Link } = Typography;
+
+
 export default function SortButtons( { setSortType } ) {
     return (
         <div className="sort-buttons">
-            <p>Sort by Date</p>
-            <button onClick={() => setSortType(Constants.DATE_FILTER_DIRECTION_DOWN)}><img src={upArrow} className="icon" alt="Sort by down date" /></button>
-            <button onClick={() => setSortType(Constants.DATE_FILTER_DIRECTION_UP)}><img src={downArrow} className="icon icon-up" alt="Sort by up date" /></button>
+             <Text>Sort by date</Text>
+             <ArrowUpOutlined onClick={() => setSortType(Constants.DATE_FILTER_DIRECTION_DOWN)} style={{color: '#f5222d', fontSize: '24px'}} />
+             <ArrowUpOutlined onClick={() => setSortType(Constants.DATE_FILTER_DIRECTION_UP)} style={{color: '#1890ff', fontSize: '24px'}} rotate={180} />
         </div>
     )
 }
